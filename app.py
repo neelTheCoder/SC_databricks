e5bd7020fc2a82f292a78cbabb6ce686b014fad8
Theo Chiang <32378236+JTC255@users.noreply.github.com>
2026-02-23 22:06:20 -0800

    Update app.py template folder

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent
 RUNS_DIR = BASE_DIR / "runs"  # each request gets a unique run folder
 RUNS_DIR.mkdir(exist_ok=True)
 
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 app.config["MAX_CONTENT_LENGTH"] = 250 * 1024 * 1024  # 250MB total upload cap
 
 
